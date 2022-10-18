43668cbdbc4cc2130daf5986c4806e992c7f8b5e
Christopher Prado <cprad18@gmail.com>
2022-10-18 14:37:17 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Christopher Prado
+//09-07-2022 2pm
+//Mini Challenge #1 "What is your name?"
+//This is a console project that will ask our user their name
+//And report it back to the console
+//Peer Review by: (enter name)
+
+Console.Clear ();
+
+
+Console.WriteLine ("What is your name?");
+
+string myName = Console.ReadLine();
+
+Console.WriteLine ("Good Afternoon" + myName);
+
+
+
+
